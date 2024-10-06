3c9a8ac4c534f16d6fdc563d984b2834dc3b84c9
Fulgurance <zohran.londais@gmail.com>
2024-10-06 11:48:25 +0100

    Updated installation process

--- Firefox/115.2.0/115.2.0.cr
+++ Firefox/115.2.0/115.2.0.cr
@@ -4,8 +4,6 @@ class Target < ISM::Software
         super
 
         mozconfigData = <<-CODE
-        ac_add_options --target=#{Ism.settings.systemTarget}
-        ac_add_options --with-toolchain-prefix=#{Ism.settings.systemTarget}-
         ac_add_options --enable-bootstrap
         ac_add_options #{option("Wireless-Tools") ? "--enable-necko-wifi" : "--disable-necko-wifi"}
         ac_add_options --enable-pulseaudio
